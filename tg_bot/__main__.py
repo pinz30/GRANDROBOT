84084dcc9ad6525e07581babf16a0d2379e1cec3
pinz30 <83577888+pinz30@users.noreply.github.com>
2021-05-11 13:47:52 +0700

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -26,7 +26,7 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 
 PM_START_TEXT = """
-**Hi, nama saya Tante Gabut !**
+**Hi {username}, nama saya Tante Gabut !**
 Saya adalah robot pintar yang dibuat khusus untuk menjaga dan meramaikan grupchat. 
 
 Tambahkan saya ke grupmu dan atur saya sebagai Admin (Edit -> Admins -> tambahkan admin) dan biarkan saya mengguncang grupmu!
